a50ef08ea0795ba193bb77294afb0e9a44c484d8
Taemin Moon <tmmoond8@gmail.com>
2019-01-26 14:13:14 +0900

    - 정적 파일 추가

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, './dist')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/cubecode', cubeCode);
